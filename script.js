3ae4399883c7b0ceac1413ad529a6678347f4345
Najib Adan <najibadan1@gmail.com>
2018-12-12 12:41:25 +0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,16 @@
+let grid = document.querySelector('.test');
+grid.setAttribute('style','display: inline-grid;grid-template-columns: repeat(16,1fr);margin: 0 auto;width: 600px;height:600px;');
+for (i=0;i<(16*16);i++){
+    let div = document.createElement('div');
+    div.classList.add('content');
+    div.setAttribute('style','border: 1px solid;')
+    div.textContent = 'text';
+    grid.appendChild(div);
+}
+
+let div = document.querySelectorAll('.content');
+div.forEach((square) => {
+    square.addEventListener('mouseover',(e)=>{
+        square.style.background = 'blue';
+    });
+});
